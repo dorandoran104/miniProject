a02326fa1b2e3200583e9f07b35b0fa08f77d1b1
dorandoran104 <pgh_96@naver.com>
2025-01-14 18:03:26 +0900

    commit

--- test/app/routes/ask_router.py
+++ test/app/routes/ask_router.py
@@ -1,34 +1,57 @@
 from fastapi import APIRouter, File, UploadFile, Form
 
 from service import ask_service
+from langchain_teddynote import logging
 
 import time
+import os
 
 ask = APIRouter()
+
+logging.langsmith("LAG_TEST")
 
 @ask.post("/question")
 async def ask_route(file: UploadFile = File(...), question: str = Form(...)):
 
-    read_time = time.time()
-    docs = await ask_service.read_pdf(file)
-    read_end_time = time.time()
-    print(f"Read time: {read_end_time - read_time}")
+    file_name = file.filename
 
-    embedding_time = time.time()
-    embedding = await ask_service.create_embedding()
-    embedding_end_time = time.time()
-    print(f"Embedding time: {embedding_end_time - embedding_time}")
-    
-    #검색기 생성
-    create_vector_store_time = time.time()
-    vector_store = await ask_service.create_vector_store(docs, embedding)
-    create_vector_store_end_time = time.time()
-    print(f"Create vector store time: {create_vector_store_end_time - create_vector_store_time}")
-    # retriever = vector_store.as_retriever()
+    if not os.path.exists(f"vector_store/{file_name}"):
+
+        read_time = time.time()
+        docs = await ask_service.read_pdf(file)
+        read_end_time = time.time()
+        print(f"Read time: {read_end_time - read_time}")
+
+        embedding_time = time.time()
+        embedding = await ask_service.create_embedding()
+        embedding_end_time = time.time()
+        print(f"Embedding time: {embedding_end_time - embedding_time}")
+
+        # 텍스트 분리
+        split_time = time.time()
+        split_docs = await ask_service.split_pdf(docs)
+        split_end_time = time.time()
+        print(f"Split time: {split_end_time - split_time}")
+        
+        #검색기 생성
+        create_vector_store_time = time.time()
+        vector_store = await ask_service.create_vector_store(split_docs, embedding,file_name)
+        create_vector_store_end_time = time.time()
+        print(f"Create vector store time: {create_vector_store_end_time - create_vector_store_time}")
+    else:
+        embedding_time = time.time()
+        embedding = await ask_service.create_embedding()
+        embedding_end_time = time.time()
+        print(f"Embedding time: {embedding_end_time - embedding_time}")
+
+        vector_store_load_time = time.time()
+        vector_store = await ask_service.load_vector_store(file_name,embedding)   
+        vector_store_load_end_time = time.time()
+        print(f"Vector store load time: {vector_store_load_end_time - vector_store_load_time}")
 
     #검색
     ask_time = time.time()
-    result = await ask_service.search_vector_store(question,vector_store)
+    result = await ask_service.search_vector_store(question,vector_store,embedding)
     ask_end_time = time.time()
     print(f"Ask question time: {ask_end_time - ask_time}")
     print(result)
--- test/app/service/ask_service.py
+++ test/app/service/ask_service.py
@@ -4,14 +4,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmb
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-from langchain.chains import LLMChain
-from langchain.chains import RetrievalQA
-from langchain.chains import AnalyzeDocumentChain
-from langchain.retrievers import ContextualCompressionRetriever
-from langchain.retrievers.document_compressors import LLMChainExtractor
 from langchain.retrievers.multi_query import MultiQueryRetriever
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from fastapi import UploadFile
+from langchain.vectorstores import Chroma
+
 import tempfile
 import dotenv
 import os
@@ -39,9 +36,16 @@ async def read_pdf(file: UploadFile):
 
         loader = PyPDFLoader(temp_file_path)
         pages = []
+        # pages.extend(loader.load())
         async for page in loader.alazy_load():
             pages.append(page)
         return pages
+        return pages
+
+async def split_pdf(docs):
+    text_spliter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=100)
+    docs = text_spliter.split_documents(docs)
+    return docs
     
 #임베딩 생성
 async def create_embedding():
@@ -51,45 +55,72 @@ async def create_embedding():
     # return vector_store.similarity_search(question,k=3)
 
 #벡터 저장소 생성
-async def create_vector_store(docs, embeddings):
-    vector_store = InMemoryVectorStore.from_documents(docs, embeddings)
+async def create_vector_store(docs, embeddings,file_name):
+    # vector_store = InMemoryVectorStore.from_documents(docs, embeddings)
+    vector_store = Chroma.from_documents(
+        documents=docs,
+        embedding=embeddings,
+        persist_directory=f"vector_store/{file_name}",
+        collection_name=file_name
+    )
+    vector_store.persist()
+    return vector_store
+
+#벡터 저장소 로드
+async def load_vector_store(file_name,embeddings):
+    vector_store = Chroma(
+        persist_directory=f"vector_store/{file_name}",
+        collection_name=file_name,
+        embedding_function=embeddings
+        )
+    
     return vector_store
 
 #벡터 저장소 검색
-async def search_vector_store(question,vector_store): 
+async def search_vector_store(question,vector_store,embeddings): 
     llm = ChatGoogleGenerativeAI(model="models/gemini-1.5-flash-001",config=generation_config)
-    
-    retriever_from_llm = MultiQueryRetriever.from_llm(llm=llm,retriever=vector_store.as_retriever())
-   
-
-    logging.basicConfig()
-    logging.getLogger('langchain.retrievers.multi_query').setLevel(logging.INFO)
-
-    unique_docs = retriever_from_llm.get_relevant_documents(query=question)
-
-    print(len(unique_docs))
-    print()
-
-    for i in unique_docs:
-        print('-'*20)
-        print(i)
-        print('-'*20)
+    retriever = vector_store.as_retriever(
+        embedding_function=embeddings
+        ,search_kwargs={"k": 2}
+        ,search_type="mmr"
+        )
+    retriever_from_llm = MultiQueryRetriever.from_llm(llm=llm,retriever=retriever)
 
     prompt = PromptTemplate.from_template(
         """
         PDF 문서에서 주어진 질문에 대한 답변을 제공하는 프롬프트입니다.
+        주어진 문서는 텍스트와 표로 구성되어 있습니다.
+        표 항목도 있으므로 표 항목은 주변 텍스트를 확인하고 답변해주세요.
+        숫자로 이루어진 표는 되도록 합계쪽을 읽고 대답해주세요
         주어진 문서를 참고하여 질문에 대한 자세한 답변을 제공하고, 관련된 경우 목차를 포함하여 추가적인 지침을 제공합니다.
-        답변은 한국어로만 제공됩니다.
-        숫자로 이뤄진 표 항목도 있으므로 표 항목은 형태를 확인하고 답변해주세요.
-
-        #Context: 
+        답변은 너무 길지 않게 요약해서 한국어로만 대답해주세요
+        
+        #Contents:
         {context}
 
         #Question:
         {question}
 
         #Answer:"""
-        )
+    )
+
+    # prompt = PromptTemplate.from_template(
+    #     """
+    #     PDF 문서에서 주어진 질문에 대한 답변을 제공하는 프롬프트입니다.
+    #     주어진 문서를 참고하여 질문에 대한 자세한 답변을 제공하고, 관련된 경우 목차를 포함하여 추가적인 지침을 제공합니다.
+        
+    #     표 항목도 있으므로 표 항목은 주변 텍스트를 확인하고 답변해주세요.
+    #     답변은 한국어로만 제공됩니다.
+    #     너무 길지 않게 요약해서 답변해주세요
+
+    #     #Context: 
+    #     {context}
+
+    #     #Question:
+    #     {question}
+
+    #     #Answer:"""
+    #     )
     chain = (
         {
             "context": retriever_from_llm,
@@ -101,5 +132,4 @@ async def search_vector_store(question,vector_store):
     )
 
     result = chain.invoke(question)
-    print(chain.invoke(question))
     return result
